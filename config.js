887d65ff44325340b7a8b35bca756e637a5af265
josephine-h-albert <josephine.h.albert@gmail.com>
2026-09-21 16:14:41 -0400

    Create config.js
    Added URL to separate file.

---
+++ config.js
@@ -0,0 +1,3 @@
+Paste your Apps Script web app URL between the quotes (it ends in /exec)
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyreF1LAwoRFb4em01FRL5GOefRTTX2U49cvkpgx4udsSs3Ud77ALSPfZO2B2cqoFrt/exec';
+ 
